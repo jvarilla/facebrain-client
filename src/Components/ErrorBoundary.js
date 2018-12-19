2947cdd52060dbc87f93fe7a02cb28bf0aeaf6fb
Joseph G. Varilla <jvarilla981@gmail.com>
2018-12-18 23:43:04 -0600

    Putting on GitHub

---
+++ src/Components/ErrorBoundary.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hasError: false
+		}
+	}
+
+	render() {
+		
+	}
+}
+
+export default ErrorBoundary;
